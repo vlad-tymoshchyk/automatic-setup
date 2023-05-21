ca6b1d8bb5f5609052cae582b23781ba181caccd
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2023-05-21 23:47:49 +0300

    ci: add checks

--- utils.bash
+++ utils.bash
@@ -1,5 +1,5 @@
-ITick='\xE2\x9C\x93'
-ICross='\xe2\x9c\x97'
+ITick='✓'
+ICross='✗'
 
 _expect_exists() {
   local code=0
@@ -7,6 +7,22 @@ _expect_exists() {
   for filepath in "$@"
   do
     if [ -f $filepath ]; then
+      echo "$ITick $filepath"
+    else
+      code=1
+      echo "$ICross $filepath"
+    fi
+  done
+
+  return $code
+}
+
+_expect_dir_exists() {
+  local code=0
+
+  for filepath in "$@"
+  do
+    if [ -d $filepath ]; then
       echo "$ITick $filepath"
     else
       code=1
